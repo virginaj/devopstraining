86ad07ecdd1e365a96da8548829576c623a8791e
Chandan Kumar <chandank.kumar@gmail.com>
2016-08-06 10:28:00 -0400

    Added session 1 file handling and string operations

--- hello.py
+++ hello.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python 
 
-print "Hello World"
+
+def foo():
+	f = open('/var/log/kern.log')
+	lines = f.readlines()
+	for line in lines:
+		print int(line.split()[2].split(':')[2]) * 10
+		
+
+
+def bar():
+	print("World")
+
+
+''' 
+###  First Int
+
+i = 10
+st = "Helo"
+l = ['a','b','c']
+dic= {'language':'Python', 'editor':'vi'}
+'''
+foo()
+bar()
