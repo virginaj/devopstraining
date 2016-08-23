e37006f54b9f358ae311de548fb2dd4d82a5d910
fwajih <faisalwajih@gmail.com>
2016-08-22 22:51:04 -0400

    comment for h2

---
+++ h2.py
@@ -0,0 +1,8 @@
+# This program says hello and ask for my name
+
+print('Hello world')
+print ('what is your name') # ask for their name
+my = raw_input()
+i
+thanks
+print ('it is a good time to meet you ' + my)
