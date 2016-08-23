15d8f4a1436a88c0e52f1b7a8b06fd24383d17de
fwajih <faisalwajih@gmail.com>
2016-08-22 22:44:55 -0400

    just a isecond test file

---
+++ h1.py
@@ -0,0 +1,6 @@
+# This program says hello and ask for my name
+
+print('Hello world')
+print ('what is your name') # ask for their name
+my = raw_input()
+print ('it is a good time to meet you ' + my)
