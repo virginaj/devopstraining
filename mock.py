520870a6a14685240b5fd4b56b606187b3c86a6e
ahsanalikhan84 <ahsanakhan@msn.com>
2016-08-27 06:32:13 -0700

    0827

--- mock.py
+++ mock.py
@@ -32,7 +32,7 @@ def clt():
 	ssh=paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect( '192.168.0.13' , username='ahsan', password='ahsankhan')
-	ins, out, err = ssh.exec_command("mkdir /opt/google ; cd /opt/google " )
+	ins, out, err = ssh.exec_command("mkdir /opt/google" )
 	
 	e=err.read()
 	if e == []:
