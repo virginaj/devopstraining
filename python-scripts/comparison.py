6e9f5469caab443276ebe2228ab6a935a34b52a8
Godfred <godfred892@gmail.com>
2016-09-06 22:23:33 -0400

    Ansible assignment Sep7 2016

--- python-scripts/comparison.py
+++ python-scripts/comparison.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-i=5
+i=4
 if i == 4:
 	print('match')
 else:
@@ -9,13 +9,13 @@ if s == " This is a test":
 	print("Match")
 else:
 	print("Failed to match")
-l = [1,2,3,4,5,6,6,76,7]
+l = [11,2,3,4,5,6,6,76,7]
 if l == [1,2,3,4,5,6,6,76,7]: 
 	print('***found IT ***')
 else:
 	print ('Not Found')
 def foo():
-	return[11,22,33,44]
+	return[11,22,33,99,44]
 if 99 in foo():
 	print('GOOD')
 else:
