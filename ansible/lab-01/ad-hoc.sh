d2c83b8d09e08b9d411304337ad465d8642135c0
Chandan Kumar <chandank@becloudready.com>
2018-09-03 18:23:42 -0500

    added files

--- ansible/lab-01/ad-hoc.sh
+++ ansible/lab-01/ad-hoc.sh
@@ -3,15 +3,15 @@
 ## Test Ad hoc commands and ssh command comparison
 
 ssh ansible-node "hostname -f"
-ansible ansible-node  -i ../inventory/ansible-nodes -a "hostname -f"
+ansible ansible-node   -a "hostname -f"
 
 ssh ansible-node "mkdir /home/ec2-user/mytestdir"
-ansible ansible-node  -i ../inventory/ansible-nodes -a "mkdir /home/ec2-user/testdirAnsible"
-ansible ansible-node -i ../inventory/ansible-nodes -m copy -a "src=inventory/ansible-nodes dest=/tmp/ansible-nodes"
-ansible ansible-node -i ../inventory/ansible-nodes -a "cat /tmp/ansible-nodes" 
-ansible ansible-node -i ../inventory/ansible-nodes -b -m yum -a "name=httpd state=present"
-ansible ansible-node -i ../inventory/ansible-nodes -b -m yum -a "name=git state=present"
-ansible ansible-node -i ../inventory/ansible-nodes -b -m git -a "repo=https://github.com/becloudready/prometheus_monitoring.git dest=/tmp version=HEAD"
+ansible ansible-node  -a "mkdir /home/ec2-user/testdirAnsible"
+ansible ansible-node -m copy -a "src=inventory/ansible-nodes dest=/tmp/ansible-nodes"
+ansible ansible-node -a "cat /tmp/ansible-nodes" 
+ansible ansible-node -b -m yum -a "name=httpd state=present"
+ansible ansible-node -b -m yum -a "name=git state=present"
+ansible ansible-node -b -m git -a "repo=https://github.com/becloudready/prometheus_monitoring.git dest=/tmp version=HEAD"
 
 # Using Ansible modules in ad-hoc commands
 
